1dc0b9e853d5130a61d7fbeccf787c12b3545382
HassanKhan123 <hk2602809@gmail.com>
2020-10-17 21:21:30 +0500

    header component added

--- layouts/users.vue
+++ layouts/users.vue
@@ -1,10 +1,19 @@
 <template>
 <div>
-<Header>USERS</Header>
+    <Header />
     <Nuxt />
 </div>
 </template>
 
+<script>
+import Header from '../components/Header'
+export default {
+    components: {
+        Header
+    }
+}
+</script>
+
 <style>
-    
+
 </style>
