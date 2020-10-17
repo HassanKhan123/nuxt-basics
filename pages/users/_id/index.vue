e40b27a3411b41a8bdefdaab5e099b8c54898d80
HassanKhan123 <hk2602809@gmail.com>
2020-10-17 16:42:16 +0500

    validating parameters

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -3,3 +3,11 @@
     Single user with ID {{$route.params.id}}
 </div>
 </template>
+
+<script>
+export default {
+    validate(data) {
+        return /^\d+$/.test(data.params.id)
+    }
+}
+</script>
